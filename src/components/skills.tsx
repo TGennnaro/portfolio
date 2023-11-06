60d0649f299610727c953091c570f4f0195a7a5c
Tyler Gennaro <tylergennaro10@gmail.com>
2023-11-06 00:39:56 -0500

    Update skills component styles

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -11,7 +11,7 @@ export function Skill({
 		advanced: 'w-full bg-green-500',
 	};
 	return (
-		<div className='relative lg:col-span-4 border border-zinc-800 p-4 flex flex-col gap-4 rounded-xl overflow-hidden rotating-border'>
+		<div className='relative lg:col-span-4 border border-zinc-800 p-4 flex flex-col gap-4 rounded-xl overflow-hidden bg-[rgba(255,255,255,0.04)] hover:bg-accent/20 hover:border-accent/40 transition-colors'>
 			<span className='text-3xl font-bold'>{name}</span>
 			<div className='w-full h-2 bg-zinc-600 rounded-lg'>
 				<div className={`${levelClass[level]} h-full rounded-lg`}></div>
@@ -31,8 +31,11 @@ export function SkillSection({
 	children: React.ReactNode;
 }) {
 	return (
-		<div className='mt-12'>
-			<h2 className='text-3xl mb-6 font-bold'>{title}</h2>
+		<div className='mt-20'>
+			<div className='text-3xl mb-8 font-bold flex items-center'>
+				<h2 className='shrink-0'>{title}</h2>
+				<hr className='ml-8 w-full border-zinc-700' />
+			</div>
 			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-4'>
 				{children}
 			</div>
